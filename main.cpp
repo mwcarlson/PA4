8ff8eb46e1274b93802a73e0330bc9aeb653a703
hp12324 <martijn.oostrom@wsu.edu>
2015-12-06 19:08:38 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,6 @@
 
 using namespace std;
 
-/*#include <cstdlib>
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include "Graph.h"
-#include "Vertex.h"
-using namespace std;*/
-
 Graph graphTest()
 {
 	//populate graph
@@ -25,68 +14,76 @@ Graph graphTest()
 	cout << "Enter a name to read graph info from: " << endl;
 	getline(cin, curstring);
 	ifstream input(curstring);
+	//I used http://www.cplusplus.com/forum/general/109435/ to help me with this next line (with some modification)
 	string curspot((std::istreambuf_iterator<char>(input)), std::istreambuf_iterator<char>());
+	//curspot now has entire input file as 1 massive string
 	maxsize = curspot.size();
 	cout << curspot << endl;
 	
 	while (curspot[i+1] != ' ')
 	{
-		
+		//still going through list of vertices
 		if (curspot[i]!='\n')
 		{
+			//We need to add a edge
 			vertices.push_back(Vertex{});
-			cout << "Adding vertex number: " << curspot[i] << endl;
+			
 		}
 		
 		i++;
 	}
-	cout << "i is " << i << " and maxsize is " << maxsize << endl;
+	
 	while (i < maxsize)
 	{
-		//cout << "i is " << i << " and curspot is " << curspot[i] << endl;
+		//Still have pieces of the edge info to process
 		if (curspot[i] == ' ')
 		{
 			if (focusmod == 0)
 			{
-				cout << "Modstring " << modstring << endl;
+				//modstring now represents the originating node of the vector
+				
 				fromvec = stoi(modstring);
 			}
 			else if (focusmod == 1)
 			{
-				cout << "Modstring " << modstring << endl;
+				//modstring now represents the ending node of the vector
 				tovec = stoi(modstring);
 			}
+			//Change what it is we're reading (from, to, weight), and clean string
 			focusmod = (focusmod + 1) % 3;
 			modstring.clear();
 		}
 		else if (curspot[i] == '\n')
 		{
-			
-			cout << "Modstring " << modstring << endl;
+			//modstring now represents the weight of the edge 
 			weightedge = stoi(modstring);
-			cout << "From vec: " << fromvec << " to vec: " << tovec << " with weight " << weightedge << endl;
-			//vertices[0].addEdge(&vertices[1], 4);
+			//smash on the new edge
 			vertices[fromvec].addEdge(&vertices[tovec], weightedge);
 			modstring.clear();
 			focusmod = (focusmod + 1) % 3;
 		}
 		else
 		{
+			//Need to add value to modstring
 			modstring += curspot[i];
 		}
 		i++;
 	}
+	//V I had a slight off-by 1 error, so these next few lines just does what my case '\n' does.
 	weightedge = stoi(modstring);
-	cout << "From vec: " << fromvec << " to vec: " << tovec << " with weight " << weightedge << endl;
-	//vertices[0].addEdge(&vertices[1], 4);
+	//cout << "From vec: " << fromvec << " to vec: " << tovec << " with weight " << weightedge << endl;
+	
 	vertices[fromvec].addEdge(&vertices[tovec], weightedge);
 	modstring.clear();
+	//V  Adds all of the vertices to the graph
 	for (auto vertex : vertices)
 	{
 		graph.addVertex(vertex);
 	}
 	return graph;
-	/*unordered_map<Vertex, int> distances = graph.computeShortestPath(&vertices[0]);
+	
+	/*The below code is just a bunch of testing statements that aren't entirely related to the code. Feel free to ignore
+	unordered_map<Vertex, int> distances = graph.computeShortestPath(&vertices[0]);
 
 	cout << "Distance from 0 to 0: " << distances[vertices[0]] << " (expected: 0)" << endl;
 	cout << "Distance from 0 to 1: " << distances[vertices[1]] << " (expected: 4)" << endl;
@@ -124,4 +121,5 @@ int main(int argc, char* argv[])
 {
 	Graph maingraph{};
 	maingraph=graphTest();
+	//maingraph now has the graph with vertices and edges that we'll use for the rest of this PA
 }
