0fc9e4921060ccde8c8618e46a5c8c51b8ab87ef
hp12324 <martijn.oostrom@wsu.edu>
2015-12-06 21:13:59 -0800

    Update Network.h

--- Network.h
+++ Network.h
@@ -1,6 +1,45 @@
 #ifndef NETWORK_H
 #define NETWORK_H
-
 #include "PA4.h"
+#include "Message.h"
+#include "Graph.h"
+class Network
+{
+private:
+	Graph graph{};
+	int tickcount = 0;
+	Message message{};
+public:
+	void onetick()
+	{
+		tickcount++;
+	}
+	void setgraph(Graph a)
+	{
+		graph = a;
+	}
+	void settick(int b)
+	{
+		tickcount = b;
+	}
+	void setmessage(Message c)
+	{
+		message = c;
+	}
+	Graph getgraph()
+	{
+		return graph;
+	}
+	int gettick()
+	{
+		return tickcount;
+	}
+	Message getmessage()
+	{
+		return message;
+	}
 
+
+
+};
 #endif
